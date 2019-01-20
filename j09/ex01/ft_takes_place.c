4d05cd9abfd96053590baeee206e566b8df8f9d6
James Chao <jchao@e1z1r8p1.42.us.org>
2019-01-19 21:39:39 -0800

    testing

--- j09/ex01/ft_takes_place.c
+++ j09/ex01/ft_takes_place.c
@@ -1,39 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_takes_place.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jchao <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/18 15:19:26 by jchao             #+#    #+#             */
+/*   Updated: 2019/01/18 15:23:40 by jchao            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-void ft_takes_place(int hour)
+void	ft_takes_place(int hour)
 {
-    printf("THE FOLLOWING TAKES PLACE BETWEEN ");
-    if (hour <= 11 || hour == 24) {
-        if (hour == 24) {
+	printf("THE FOLLOWING TAKES PLACE BETWEEN ");
+    if (hour <= 11 || hour == 24)
+	{
+        if (hour == 24)
+		{
             printf("%d A.M. AND ", hour - 12);
-        } else {
+        } else
+		{
             printf("%d A.M. AND ", hour);
         }
-    } else {
-        if (hour == 12) {
+    }
+	else
+	{
+        if (hour == 12)
+		{
             printf("%d P.M. AND ", hour);
-        } else {
+        }
+		else
+		{
             printf("%d P.M. AND ", hour - 12);
         }
     }
     hour = hour % 24;
-    if (hour + 1 <= 11 || hour + 1 == 24) {
-        if (hour + 1 == 24) {
+    if (hour + 1 <= 11 || hour + 1 == 24)
+	{
+        if (hour + 1 == 24)
+		{
             printf("%d A.M.\n", hour + 1 - 12);
-        } else {
+        }
+		else
+		{
             printf("%d A.M.\n", hour + 1);
         }
-    } else {
-        if (hour + 1 == 12) {
+    }
+	else
+	{
+        if (hour + 1 == 12)
+		{
             printf("%d P.M.\n", hour + 1);
-        } else {
+        }
+		else
+		{
         printf("%d P.M.\n", hour + 1 - 12);
         }
     }
-}
-
-int main()
-{
-    ft_takes_place(24);
-    return (0);
 }
